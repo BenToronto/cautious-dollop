732576f777469b441a5b3dccfb0175b05615ec8c
BenToronto <83716543+BenToronto@users.noreply.github.com>
2022-01-14 10:54:38 -0500

    Create screen.h

---
+++ screen.h
@@ -0,0 +1,45 @@
+#include "screen.cpp"
+printf("console\n", );
+function clickbutton() {
+  do {
+    fprintf(startgame, "click the button\n", );
+     int main(int argc, char const *argv[]) {
+      get.clickbutton(button);
+      button();
+      case 5:
+      #ifndef SOMASTHEPLAYER
+      #define SOMASTHEPLAYER
+      
+      typedef std::shared_ptr<massofplayer_t> my_type_ptr;
+      switch (xyz) {
+        case 4:
+        struct xyz {
+          button.getui();
+          int massofplayer_t(int argc, char const *argv[]) {
+            if (cpp.getif(5)) {
+              if (cpp(if.cpp(1))) {
+                printf("no button\n", );
+              } else if (clickbutton(startgame)) {
+                g=typedef int clickbutton;
+                switch (typedef int Type;) {
+                  case 1:
+                  int main(int argc, char const *argv[]) {
+                    nass();
+                    return 0;
+                  }
+                }
+              } else {
+                get.button("no button");
+                bracket();
+              }
+            }
+            return 0;
+          }
+        };
+      }
+      #endif /* end of include guard: SOMASTHEPLAYER */
+      
+      return 0;
+    }
+  } while(main(mui"k"{k||jh}));
+}
